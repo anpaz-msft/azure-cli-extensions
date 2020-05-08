a8a5f8dc1ccecda8566029340ca3bbbc86d6f8c5
Andres Paz <anpaz@microsoft.com>
2020-05-07 22:08:12 -0700

    offers list

--- src/quantum/azext_quantum/_client_factory.py
+++ src/quantum/azext_quantum/_client_factory.py
@@ -20,5 +20,8 @@ def cf_quantum_mgmt(cli_ctx, *_):
 def cf_workspaces(cli_ctx, *_):
     return cf_quantum_mgmt(cli_ctx).workspaces
 
+def cf_offerings(cli_ctx, *_):
+    return cf_quantum_mgmt(cli_ctx).offerings
+
 def cf_jobs(cli_ctx, subscription_id=None, resource_group_name=None, workspace_name=None):
     return cf_quantum(cli_ctx, subscription_id, resource_group_name, workspace_name).jobs
--- src/quantum/azext_quantum/commands.py
+++ src/quantum/azext_quantum/commands.py
@@ -5,21 +5,28 @@
 
 # pylint: disable=line-too-long
 from azure.cli.core.commands import CliCommandType
-from azext_quantum._client_factory import cf_workspaces, cf_jobs
+from azext_quantum._client_factory import cf_offerings
 
 def load_command_table(self, _):
 
     workspace_ops = CliCommandType(operations_tmpl='azext_quantum.operations.workspace#{}')
+    offerings_ops = CliCommandType(
+        operations_tmpl='azext_quantum.vendored_sdks.azure_mgmt_quantum.operations.offerings_operations#OfferingsOperations.{}',
+        client_factory=cf_offerings
+        )
     # target_ops = CliCommandType(operations_tmpl='azext_quantum.operations.target#{}')
     # job_ops = CliCommandType(operations_tmpl='azext_quantum.operations.job#{}')
 
     with self.command_group('quantum workspace', workspace_ops) as w:
         w.command('list', 'list')
-        w.command('show', 'show')
+        w.command('show', 'show')   ## TODO: argument list/help
         # w.command('set', 'set')
         # w.command('get', 'get')
         # w.show_command('show', 'get')
         # w.custom_command('set', 'get')
+
+    with self.command_group('quantum offers', offerings_ops) as w:
+        w.command('list', 'list')   ## TODO: argument list/help
 
     # with self.command_group('quantum target', target_ops) as t:
     #     t.command('list', 'list_quantum')
